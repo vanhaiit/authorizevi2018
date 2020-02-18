ce5ebcb9d19c97f8a6aa556cbb81e99994fcb01c
vanhaiit <vanhaiit2018@gmail.com>
2020-02-18 17:32:44 +0700

    add

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
     entry: resolve(__dirname, 'src/index.js'),
     output: {
         path: resolve(__dirname, 'dist'),
-        filename: 'authorvi2018.js',
-        library: 'authorvi2018'
+        filename: 'authorizevi2018.js',
+        library: 'authorizevi2018'
     }
 }
